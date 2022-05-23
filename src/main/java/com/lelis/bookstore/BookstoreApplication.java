20837a516dd5949b307439312d7a4bca1811ff4d
RonaldoLelis <delelis.locutor@gmail.com>
2022-05-23 16:08:22 -0300

    subindo projeto inicial

---
+++ src/main/java/com/lelis/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.lelis.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lelis/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lelis.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
