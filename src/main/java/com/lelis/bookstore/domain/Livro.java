83b248af5995a70d3ba331e8b96f66f0fe02b1d2
RonaldoLelis <delelis.locutor@gmail.com>
2022-05-23 21:59:20 -0300

    ajustando as tabelas no banco

--- src/main/java/com/lelis/bookstore/domain/Livro.java
+++ src/main/java/com/lelis/bookstore/domain/Livro.java
@@ -31,12 +31,13 @@ public class Livro implements Serializable  {
 		super();
 	}
 
-	public Livro(Integer id, String titulo, String nome_autor, String texto) {
+	public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
 		super();
 		this.id = id;
 		this.titulo = titulo;
 		this.nome_autor = nome_autor;
 		this.texto = texto;
+		this.categoria = categoria;
 	}
 
 	public Integer getId() {
--- src/main/java/com/lelis/bookstore/service/DBService.java
+++ src/main/java/com/lelis/bookstore/service/DBService.java
@@ -23,11 +23,11 @@ public class DBService {
 		Categoria cat2 = new Categoria(null, "Ficcção Científica", "Ficcção científica");
 		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
 				
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum");
-		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum");
-		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum");
-		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum");
-		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum");
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
 				
 		cat1.getLivros().addAll(Arrays.asList(l1, l2));
 		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
@@ -35,5 +35,4 @@ public class DBService {
 		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
 	}
-
 }
