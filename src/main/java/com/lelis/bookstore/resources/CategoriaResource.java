faab2dfb099eefba081a310b0179e1f8329ac5fe
RonaldoLelis <delelis.locutor@gmail.com>
2022-05-28 11:47:24 -0300

    criação do método POST Categoria

--- src/main/java/com/lelis/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/lelis/bookstore/resources/CategoriaResource.java
@@ -1,13 +1,21 @@
 package com.lelis.bookstore.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lelis.bookstore.domain.Categoria;
+import com.lelis.bookstore.dtos.CategoriaDTO;
 import com.lelis.bookstore.service.CategoriaService;
 
 @RestController
@@ -21,5 +29,19 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
