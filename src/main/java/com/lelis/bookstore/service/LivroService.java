3b6633e175a3aceae04141226d0ec273bd713d55
RonaldoLelis <delelis.locutor@gmail.com>
2022-05-30 23:57:56 -0300

    Criação do método POST para livros

--- src/main/java/com/lelis/bookstore/service/LivroService.java
+++ src/main/java/com/lelis/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lelis.bookstore.domain.Categoria;
 import com.lelis.bookstore.domain.Livro;
 import com.lelis.bookstore.repositories.LivroRepository;
 import com.lelis.bookstore.service.exceptions.ObjectNotFoundException;
@@ -28,5 +29,24 @@ public class LivroService {
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaService.findById(id_cat);
 		return repository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
